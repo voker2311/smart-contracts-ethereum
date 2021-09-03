504ca5a597eabaff9b038f96711a44eebf953d74
Akshay Shinde <akshay.shinde@vit.edu.in>
2021-09-04 00:09:42 +0530

    Create contract1.sol

---
+++ contract1.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.4.25;
+
+contract Demo
+{
+  int value;
+  
+  constructor() public {
+    value = 100;
+  }
+  
+  function getValue() view public returns(int)
+  {
+    return value;
+  }
+  
+  function addValue(int add) public
+  {
+    value = value + add;
+  }
+  
+  function deleteValue(int del) public
+  {
+    value = value + del
+  }
+  
+}
